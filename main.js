523f8e9c75ce9547415339d9014a19d622bd7e64
giorgitsi <113306383+giorgitsi@users.noreply.github.com>
2025-02-08 22:06:32 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+let btn = document.querySelector(".btn")
+console.log(btn)
+
+// x++
+// x = x + 1
+// x += 5
+// x = x + 5
+// x = x - 2
+// x *= 3
+// x = x * 3
+// x /= 4
+// x = x / 4
+
+let x = 5
+
+btn.addEventListener("click", function () {
+    x++
+    console.log(x)
+    btn.textContent = x
+
+
+
+
+
+})
+
+// btn.textContent = "abc"
